0659713f785b767c9d4c6c4f5811fb0f60c6dd0f
Mitchell Henke <mitchh23@gmail.com>
2016-07-18 12:56:35 -0500

    remove conditional assignment

--- lib/verk_web/mount.ex
+++ lib/verk_web/mount.ex
@@ -1,7 +1,12 @@
 defmodule VerkWeb.Mount do
   defmacro __using__(path: path) do
     quote bind_quoted: [path: path] do
-      unless String.starts_with?(path, "/"), do: path = "/" <> path
+      path = unless String.starts_with?(path, "/") do
+        "/" <> path
+      else
+        path
+      end
+
       plug VerkWeb.Plug.Mount, path
       socket "#{path}/socket", VerkWeb.UserSocket
     end
